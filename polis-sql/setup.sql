c90f07ba8b1f2dee445723c7cb6c0bfdfdbd5c75
RenatoCRON <renato.cron@gmail.com>
2016-09-23 18:58:45 -0300

    new setup.sql

--- polis-sql/setup.sql
+++ polis-sql/setup.sql
@@ -26,6 +26,19 @@ update axis set name = 'nao-usado-no-polis' where id = 1;
 
 delete from axis where id > 1;
 
-update region set display_order = display_order + 100 where upper_region = 35063;
-update region set display_order = 99 where id = 35063;
-update region set display_order = 0 where id = 35054;
+update "region" set display_order=0 where id = 35063;
+update "region" set display_order=216 where id = 3550704;
+update "region" set display_order=217 where id = 3520400;
+update "region" set display_order=218 where id = 3510500;
+update "region" set display_order=219 where id = 3555406;
+update "region" set display_order=300 where id = 35054;
+update "region" set display_order=407 where id = 3537602;
+update "region" set display_order=408 where id = 3522109;
+update "region" set display_order=409 where id = 3531100;
+update "region" set display_order=410 where id = 3541000;
+update "region" set display_order=411 where id = 3551009;
+update "region" set display_order=412 where id = 3513504;
+update "region" set display_order=413 where id = 3548500;
+update "region" set display_order=414 where id = 3518701;
+update "region" set display_order=415 where id = 3506359;
+update "region" set display_order=10000000 where id = 351;
